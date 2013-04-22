ea89733d5a903dd0375c59e3aca63d20bc44aa53
stonelee <istonelee@gmail.com>
2013-04-21 23:29:12 -0800

    Update documentation

--- src/tab.js
+++ src/tab.js
@@ -1,34 +1,13 @@
 define(function(require, exports, module) {
-  var $ = require('$'),
-    Switchable = require('switchable');
+  var Switchable = require('switchable');
 
   var Tab = Switchable.extend({
     attrs: {
       triggerType: 'click',
+      classPrefix: '',
       activeTriggerClass: 'tab-hd-is-active'
-    },
-
-    setup: function() {
-      Tab.superclass.setup.call(this);
-
-      this._fixNavHeight();
-    },
-
-    //nav比原始高度少1像素，盖住strip
-    _fixNavHeight: function(){
-      var nav = this.triggers.parent();
-      if (navHeight === 0) {
-        navHeight = nav.height();
-      }
-      if (navHeight > 0) {
-        nav.height(navHeight - 1);
-      }
     }
-
   });
 
   module.exports = Tab;
-
-  var navHeight = 0;
-
 });
